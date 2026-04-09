46a4a0d93b5663e408cb4cfe691c1c2385700823
pedro sky <pedroassis100@gmail.com>
2026-04-09 02:52:19 -0300

    integração de tailwind e testes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTasks";
+
 function App() {
   return (
     <div>
-      <h1>Gerenciador de tarefas</h1>
-      <Tasks></Tasks>
+      <h1 className="text-red-500">Gerenciador de tarefas</h1>
+      <Tasks />
+      <AddTask />
     </div>
   );
 }
+
+<div className="bg-blue-500"></div>;
 
 export default App;
