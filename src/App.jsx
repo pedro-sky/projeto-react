69d3d11dadc68cff685fd053ded3b2acc6db58e7
pedro sky <pedroassis100@gmail.com>
2026-04-13 18:09:00 -0300

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,23 @@ function App() {
     },
   ]);
 
+  function onTaskClick(tasksId) {
+    const newTasks = tasks.map((tasks) => {
+      if (tasks.id == tasksId)
+        return { ...tasks, isCompleted: "tasks.isCompleted" };
+
+      return tasks;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de tarefas
         </h1>
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
         <AddTask />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,12 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((tasks) => (
         <li key={tasks.id} className="flex gap-2">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => props.onTaskClick(tasks.id)}
+            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+          >
             {tasks.title}
+            {tasks.isCompleted ? "COMPLETE" : "INCOMPLETE"}
           </button>
 
           <button className="bg-slate-400 p-2 rounded-md text-white">
