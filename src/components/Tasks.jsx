5c9aa7302ea47dd4cc4f8da49b7ba98ffbc51a19
pedro sky <pedroassis100@gmail.com>
2026-04-15 03:20:07 -0300

    task details page added, with the use of router to navigate to the page and pass the task details as query params

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(tasks) {
+    const query = new URLSearchParams();
+    query.set("title", tasks.title);
+    query.set("description", tasks.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((tasks) => (
@@ -13,7 +23,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
             {tasks.title}
           </button>
 
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(tasks)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
           <button
